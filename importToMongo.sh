a184c118744f6f8b752accea646d5256ea57dda6
hubertwojciechowski <hubertwojciechowski@hotmail.com>
2012-04-01 19:51:52 +0200

    Dodanie skryptu importujacego dane z pliku do bazy MongoDB

---
+++ importToMongo.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# Skrypt importuje dane (JSON ) z pliku do bazy mongoDB
+
+# Funkcja pokazuje sposob uzycia skrypt
+function helpme() {
+  echo "";
+  echo "Uzycie:"
+  echo "[sciezkaDoPlikuZdanymi] [nazwaBazy] [nazwa kolekcji] [port]"
+  echo "Przyklad: "
+  echo "dane.txt music hypem 27017"
+  echo "";
+  exit 1
+}
+
+# sprawdzanie ilosci parametrow
+if [ $# -ne 4 ]; then
+	helpme;
+fi
+
+# pobieranie zmiennych
+filePath=$1;
+dbName=$2;
+collectionName=$3;
+port=$4;
+
+# /clint/nosql/bin/mongodb/mongoimport -d $dbName -c $collectionName --port $port $filePath
+
+# wykoananie polecenia importu
+mongoimport -d $dbName -c $collectionName --port $port $filePath
+
+exit 0
+
+
+
+
